bd15d2e2e0c3e069b06c61fff3aea24879c8e626
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2017-04-20 19:35:44 +0200

    Remove unnecesarry imports

--- src/test/java/test_objects/Student.java
+++ src/test/java/test_objects/Student.java
@@ -1,10 +1,5 @@
 package test_objects;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Created by Mateusz on 07.04.2017.
  */
